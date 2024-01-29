719b262d204e93431862db9a38b68e4167998fda
DARKMOONlite <sebastianschroder@hotmail.com>
2024-01-29 21:11:08 +1100

    testing ability to include css elements

--- setup.bash
+++ setup.bash
@@ -1,18 +1,20 @@
-
+# update and upgrade the system
 sudo apt update
 sudo apt upgrade
 
-
+# install nginx
 sudo apt install nginx
-sudo /etc/init.d/nginx start
+
 
 
 # set nginx settings by replacing default file
 sudo mv /etc/nginx/sites-available/default /etc/nginx/sites-available/default.bak
 sudo cp ./nginx/default /etc/nginx/sites-available/default
 
-
+# copy webserver files to /var/www/cam
+sudo mkdir /var/www/cam
 sudo cp -a ./webserver/. /var/www/cam/
 
 # restart the nginx service after settings set
 sudo service nginx restart
+sudo /etc/init.d/nginx start
