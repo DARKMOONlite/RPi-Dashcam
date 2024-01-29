ebecc3863741a10b5d3e61b902f57f322e4dd853
DARKMOONlite <sebastianschroder@hotmail.com>
2024-01-29 20:24:05 +1100

    added custom nginx config rather than updating the current one. more consistet as we don't have to worry about people having edited their file

--- setup.bash
+++ setup.bash
@@ -6,4 +6,10 @@ sudo apt upgrade
 sudo apt install nginx
 sudo /etc/init.d/nginx start
 
-sed  -i 's/root \/var\/www\/html;/~\/Git\/RPi-Dashcam\/webserver;/g' /etc/nginx/sites-available/default
+
+# set nginx settings by replacing default file
+sudo mv /etc/nginx/sites-available/default /etc/nginx/sites-available/default.bak
+sudo cp ./nginx/default /etc/nginx/sites-available/default
+# sudo sed  -i 's/root \/var\/www\/html;/~\/Git\/RPi-Dashcam\/webserver;/g' /etc/nginx/sites-available/default
+# restart the nginx service after settings set
+sudo service nginx restart
