85c9a7d93f7a9ee29fb984a271c505280efe33d7
DARKMOONlite <sebastianschroder@hotmail.com>
2024-01-29 20:56:31 +1100

    now instead of changing nginx root to git repo, copying webserver contents to new folder inside /etc/www

--- setup.bash
+++ setup.bash
@@ -10,6 +10,9 @@ sudo /etc/init.d/nginx start
 # set nginx settings by replacing default file
 sudo mv /etc/nginx/sites-available/default /etc/nginx/sites-available/default.bak
 sudo cp ./nginx/default /etc/nginx/sites-available/default
-# sudo sed  -i 's/root \/var\/www\/html;/~\/Git\/RPi-Dashcam\/webserver;/g' /etc/nginx/sites-available/default
+
+
+sudo cp -a ./webserver/. /var/www/cam/
+
 # restart the nginx service after settings set
 sudo service nginx restart
